be8a5a46b1da091e505c5a1432405beb93fd0ffb
unknown <tvanrys@fanshaweonline.ca>
2020-10-14 14:02:46 -0400

    adding comments

--- js/main.js
+++ js/main.js
@@ -1,28 +1,19 @@
-// import your packages here
-import Team from "./modules/DataModule.js";
-
 (() => {
-    // stub * just a place for non-component-specific stuff
-    console.log('loaded');
-
-    //console.log(Team);
-
-    //debugger;
-
-    // select our user elements and load the content
-    // let userSection = document.querySelector(".user-section").children;
-
-    // userSection[1].textContent = Team["Justin"].name;
-    // userSection[2].textContent = Team["Justin"].role;
-    // userSection[3].textContent = Team["Justin"].nickname;
-
     // XHTTP is the old tried-and-true way of doing AJAX - still relevant, but there are better options
+    
+     // create an instance of the AJAX object
     let myReq = new XMLHttpRequest;
+
+    // add an event handler so that we can track the stages of the request and respond accordingly
     myReq.addEventListener('readystatechange', handleRequest);
 
+    // get the request ready to go / configure it with method and resource request
     myReq.open('GET', '../DataSet.json');
+    
+    // send the request off to the server
     myReq.send();
 
+    // this is a passive listener function - it gets invoked for every stage of the AJAX request. When the request is done and the data payload is returned from the server it passes that data to the handleDataSet function
     function handleRequest() {
         if (myReq.readyState === XMLHttpRequest.DONE) {
             // check status here and proceed
@@ -42,12 +33,15 @@ import Team from "./modules/DataModule.js";
 
     }
 
+    // this receives the data payload from our AJAX request, parses it (turns the returned JSON object back into a plain JavaScript object) and renders the data to our view (the markup in index.html)
     function handleDataSet(data) {
         let myData = JSON.parse(data),
             userSection = document.querySelector('.user-section'),
             userTemplate = document.querySelector('#user-template').content;
 
         debugger;
+
+        // loop through the JavaScript object and for each user, make a copy of the user template we find at the bottom of index.html, populate it with the user's data, and put that fresh copy in the users section in index.html
 
         for (let user in myData) {
             let currentUser = userTemplate.cloneNode(true),
@@ -63,13 +57,4 @@ import Team from "./modules/DataModule.js";
 
         console.log(data);
     }
-
-    // fetch is an easier syntax to remember, and is pretty well supported but still considered a "base model" way of doing AJAX
-
-    // fetch('../DataSet.json')
-    //     .then(res => res.json())
-    //     .then(data => console.log(data))
-    //     .catch((err) => {
-    //         console.error(err)
-    //     });
 })();
