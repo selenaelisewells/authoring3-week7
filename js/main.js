1bb12a994d6920b2369ef1eb1f4a245c8fd441ca
tvanrys <tvanrys@fanshaweonline.ca>
2020-11-04 17:31:20 -0500

    updates

--- js/main.js
+++ js/main.js
@@ -1,21 +1,10 @@
+import { fetchData } from "./modules/TheDataMiner.js";
+
 (() => {
-    // start with a Fetch all
-    fetch('./DataSet.json')
-        .then(res => res.json()) // parse the JSON (translate) back to plain JS
-        .then(data => {
-            // this is our data (DataSet.json)
-            // converted to a plain JavaScript object
-            handleDataSet(data);
-        })
-    .catch((error) => console.log(error));
-
-
     // this receives the data payload from our AJAX request, parses it (turns the returned JSON object back into a plain JavaScript object) and renders the data to our view (the markup in index.html)
     function handleDataSet(data) {
         let userSection = document.querySelector('.user-section'),
             userTemplate = document.querySelector('#user-template').content;
-
-        debugger;
 
         // loop through the JavaScript object and for each user, make a copy of the user template we find at the bottom of index.html, populate it with the user's data, and put that fresh copy in the users section in index.html
 
@@ -34,4 +23,7 @@
 
         console.log(data);
     }
+
+    fetchData("../DataSet.json").then(data => handleDataSet(data)).catch(err => { console.log(err); });
+
 })();
